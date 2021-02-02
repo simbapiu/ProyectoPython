2c5ce674f218b82d34b7581bbafd049a26735e0c
simbapiu <ricardo.zaldivar91@gmail.com>
2021-02-01 22:51:36 -0600

    Resolved no module error

--- proyecto.py
+++ proyecto.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly
 import plotly.express as px
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
